7c986f224f72733846d4fabef8f3ecfd0304533f
dati giorgadze <d.giorgadze2004@gmail.com>
2023-10-11 19:25:39 +0400

    Task 02 Day 03

---
+++ Day03/Task01/Task01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Enter the number of elements you want to input: ");
+        if (int.TryParse(Console.ReadLine(), out int numElements) && numElements > 0)
+        {
+            int[] numbers = new int[numElements];
+
+            for (int i = 0; i < numElements; i++)
+            {
+                Console.Write($"Enter element {i + 1}: ");
+                if (int.TryParse(Console.ReadLine(), out numbers[i]) == false)
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid integer.");
+                    i--;
+                }
+            }
+
+            Console.WriteLine("The array you entered:");
+            for (int i = 0; i < numElements; i++)
+            {
+                Console.WriteLine($" {i + 1}: {numbers[i]}");
+            }
+        }
+    }
+}
