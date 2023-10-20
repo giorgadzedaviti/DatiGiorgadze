927c24e433ae85cc02f793b83c69a63de7b4528f
dati giorgadze <d.giorgadze2004@gmail.com>
2023-10-20 19:26:12 +0400

    final code

---
+++ Day05/Task01/Task01/Rectangle.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+public class Rectangle : Shape
+{
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width)
+    {
+        if (length <= 0 || width <= 0)
+        {
+            throw new ArgumentException("lengths must be greater than 0");
+        }
+
+        this.length = length;
+        this.width = width;
+    }
+
+    public override double Perimeter()
+    {
+        return 2 * (length + width);
+    }
+
+    public override double Area()
+    {
+        return length * width;
+    }
+}
